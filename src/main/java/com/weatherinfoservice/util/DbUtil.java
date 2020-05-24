cfccd7e482600e7be4dd266eb828e4ad5d4f35d5
neerajkumar4296 <60752969+neerajkumar4296@users.noreply.github.com>
2020-05-24 21:23:30 +0530

    minor change, just to check aws pipeline notification using SNS

--- src/main/java/com/weatherinfoservice/util/DbUtil.java
+++ src/main/java/com/weatherinfoservice/util/DbUtil.java
@@ -12,9 +12,13 @@ import java.util.stream.Stream;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import com.weatherinfoservice.ApplicationsUtiltyController;
 import com.weatherinfoservice.db.repositories.EmployeeRepository;
 import com.weatherinfoservice.exceptions.BadServiceRequestException;
 import com.weatherinfoservice.model.Address;
@@ -22,34 +26,40 @@ import com.weatherinfoservice.model.AddressType;
 import com.weatherinfoservice.model.Employee;
 import com.weatherinfoservice.model.Qualification;
 
-@Component
+@Repository
 public class DbUtil {
 
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationsUtiltyController.class);
+	
 	@Autowired
 	EmployeeRepository empRepository;
 	
 		public void saveEmployeeDetails() {
 		
-			Address addresstemp= new Address("B155/303", "Sukhobrishti, Shapoorji", "Kolkata", "West Bengal", "India", 700135, AddressType.TEMPORARY);
-			Address addressperm= new Address("141", "Station Road,", "Khagaria", "Bihar", "India", 851204, AddressType.HOME);
-		    List<Address> addressesNeeraj = new ArrayList<>();
-		    addressesNeeraj.add(addresstemp);
-		    addressesNeeraj.add(addressperm);
-			
-		    Address addresstempO= new Address("B155/303", "Sukhobrishti, Shapoorji", "Kolkata", "West Bengal", "India", 700135, AddressType.TEMPORARY);
-		    Address addressPerm= new Address("58", "Thana Chowk", "Samastipur", "Bihar", "India", 848114, AddressType.HOME);		    
-			Address addressOffice= new Address("ODC4D3", "Gitanjali Park", "Newtown", "West Bengal", "India", 700156, AddressType.OFFICE);
-		    List<Address> addressesOther= new ArrayList<>();
-			addressesOther.add(addresstempO);
-			addressesOther.add(addressOffice);
-			addressesOther.add(addressPerm);
-		Employee empNeeraj= new 
-				Employee(986845L, "Neeraj", "Kumar", LocalDate.of(1995, 12, 31), 50000L, Qualification.GRADUATE, addressesNeeraj);
+			/*
+			 * Address addresstemp= new Address("B155/303", "Sukhobrishti, Shapoorji",
+			 * "Kolkata", "West Bengal", "India", 700135, AddressType.TEMPORARY); Address
+			 * addressperm= new Address("141", "Station Road,", "Khagaria", "Bihar",
+			 * "India", 851204, AddressType.HOME); List<Address> addressesNeeraj = new
+			 * ArrayList<>(); addressesNeeraj.add(addresstemp);
+			 * addressesNeeraj.add(addressperm);
+			 * 
+			 * Address addresstempO= new Address("B155/303", "Sukhobrishti, Shapoorji",
+			 * "Kolkata", "West Bengal", "India", 700135, AddressType.TEMPORARY); Address
+			 * addressPerm= new Address("58", "Thana Chowk", "Samastipur", "Bihar", "India",
+			 * 848114, AddressType.HOME); Address addressOffice= new Address("ODC4D3",
+			 * "Gitanjali Park", "Newtown", "West Bengal", "India", 700156,
+			 * AddressType.OFFICE); List<Address> addressesOther= new ArrayList<>();
+			 * addressesOther.add(addresstempO); addressesOther.add(addressOffice);
+			 * addressesOther.add(addressPerm); Employee empNeeraj= new Employee(986845L,
+			 * "Neeraj", "Kumar", LocalDate.of(1995, 12, 31), 50000L,
+			 * Qualification.GRADUATE, addressesNeeraj);
+			 * 
+			 * Employee empOther= new Employee(1073214L, "Aditya", "Gupta",
+			 * LocalDate.of(1992, 03, 24), 60000L, Qualification.GRADUATE, addressesOther);
+			 */
 		
-		Employee empOther= new 
-				Employee(1073214L, "Aditya", "Gupta", LocalDate.of(1992, 03, 24), 60000L, Qualification.GRADUATE, addressesOther);
-		
-		
+			logger.error("dummy save:: sample data is already in DB...please check code for new insertion");
 
 //				SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory();
 //				Session session = sessionFactory.openSession();
@@ -58,7 +68,7 @@ public class DbUtil {
 //				session.getTransaction().commit();
 //			    session.close();
 		
-				empRepository.saveAll(Arrays.asList(empNeeraj, empOther));
+				//empRepository.saveAll(Arrays.asList(empNeeraj, empOther));
 				//empRepository.save(empOther);
 	}
 		
